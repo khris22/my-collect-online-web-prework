5e297932ec484c761a5d7aebe3c7e1085c3d8473
Khristian Punzalan <khrispunzalan@gmail.com>
2019-06-05 00:15:17 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,7 +1,9 @@
 def my_collect(collection)
   i = 0 
   new_collection = []
-
+  my_collect(array) do |name|
+    name.split(" ").first
+end
   while i < collection.length 
   yield(collection[1])
       i += 1 
