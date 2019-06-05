66aebae03f40ab268f2bb371a23810f5652d37e5
Khristian Punzalan <khrispunzalan@gmail.com>
2019-06-05 00:08:10 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,16 @@
-
+def my_collect(collection)
+  i = 0 
+  new_collection = []
+        my_collect(collection) do |name|
+        name.split(" ").first
+      my_collect(collection) do |lang|
+        lang.upcase
+  end
+  while i < collection.length 
+  yield(collection[1])
+      i += 1 
+  end 
+  new_collection
+  end
+end
 
