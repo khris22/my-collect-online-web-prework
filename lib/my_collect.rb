4f4e92143ae0e9df4b09d8d6d673a9c119d40857
Khristian Punzalan <khrispunzalan@gmail.com>
2019-06-05 00:10:41 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,15 +1,18 @@
 def my_collect(collection)
   i = 0 
   new_collection = []
-        my_collect(collection) do |name|
-        name.split(" ").first
-      my_collect(collection) do |lang|
-        lang.upcase
-  
+
   while i < collection.length 
   yield(collection[1])
       i += 1 
   end 
+  
+  yield my_collect(collection) do |name|
+        name.split(" ").first
+    end
+  yield my_collect(collection) do |lang|
+        lang.upcase
+    end
   new_collection
   end
 end
