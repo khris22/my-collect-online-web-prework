1e3e1b56c73595676bbd9e9cb0bc9d785097c814
Khristian Punzalan <khrispunzalan@gmail.com>
2019-06-05 00:45:23 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -10,3 +10,14 @@ def my_collect(collection)
   end 
   new_collection
 end
+
+
+def my_collect(collection)
+  i = 0 
+  new_collection = []
+  while i < collection.length 
+  yield(collection[1])
+      i += 1 
+  end
+    new_collection
+end
