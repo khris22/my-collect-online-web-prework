c71eb04eece50c2a688c9742cd1e626af3b06330
Khristian Punzalan <khrispunzalan@gmail.com>
2019-06-05 00:11:40 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -7,10 +7,10 @@ def my_collect(collection)
       i += 1 
   end 
   
-  yield my_collect(collection) do |name|
+  my_collect(collection) do |name|
         name.split(" ").first
     end
-  yield my_collect(collection) do |lang|
+  my_collect(collection) do |lang|
         lang.upcase
     end
   new_collection
